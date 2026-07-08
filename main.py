f19fff1a066a93848d07df0941a73f08fee03b00
porchingman <poachingman@gmail.com>
2026-07-08 16:38:08 +0900

    키값 설정

--- main.py
+++ main.py
@@ -1,7 +1,28 @@
+import os
+
+from dotenv import load_dotenv
 from fastapi import FastAPI
+
+# .env 파일 읽기
+load_dotenv()
 
 app = FastAPI()
 
+# 환경변수 읽기
+GEMINI_API_KEY = os.getenv("GEMINI_API_KEY")
+SUPABASE_URL = os.getenv("SUPABASE_URL")
+SUPABASE_KEY = os.getenv("SUPABASE_KEY")
+
 @app.get("/")
 def root():
-    return {"message": "Hello FastAPI World from main.py!"}
+    return {
+        "message": "Hello FastAPI World from main.py!!"
+    }
+
+@app.get("/config")
+def config():
+    return {
+        "gemini_key": GEMINI_API_KEY,
+        "supabase_url": SUPABASE_URL,
+        "supabase_key": SUPABASE_KEY
+    }
